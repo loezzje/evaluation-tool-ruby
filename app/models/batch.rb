ddb81d5f268d89954b256ce4478abce43706662a
loezzje <m.demmendaal@hotmail.com>
2017-11-15 19:16:12 +0100

    add styling batches and fix random
    Signed-off-by: loezzje <m.demmendaal@hotmail.com>

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,23 +2,25 @@ class Batch < ApplicationRecord
   validates :name, presence: true
   has_many :students
 
+
   def set_random_student
     random = rand(100)
     students = self.students
     colorGroup = students.select do |student|
-      if student.evaluations[0]
-        if random <= 17
+        if !student.evaluations[0]
+          next
+        elsif random <= 17
           student.evaluations[-1].score == 'green'
-        elsif random > 17 && random <= 50
-          student.evaluations[-1].score == 'yellow'
+        elsif (random > 17) && (random <= 50)
+          student.evaluations[-1].score == 'orange'
         elsif random > 50
           student.evaluations[-1].score == 'red'
         end
-      end
     end
     noEvaluationGroup = students.select do |student|
       !student.evaluations[0]
     end
     return (colorGroup + noEvaluationGroup).sample
   end
+
 end
