e04c613afe10e63cd36dcfdf02c0c792d9c03f47
loezzje <m.demmendaal@hotmail.com>
2017-11-21 16:45:29 +0100

    display calender duration of batch

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
     @evaluations = @student.evaluations.all
     @studentId = @student.id
     @batchId = Batch.find(params[:batch_id]).id
+    @weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    @month1 = @student.set_month_one
+    @month2 = @student.set_month_two
+    @month3 = @student.set_month_three
+    @year1 = @student.set_year_one
+    @year2 = @student.set_year_two
+    @year3 = @student.set_year_three
   end
 
   def new
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,53 @@ class Student < ApplicationRecord
   belongs_to :batch
   has_many :evaluations, dependent: :destroy
 
+  def set_month_one
+    start_date = self.batch.start_date
+    return start_date.mon
+  end
+
+
+  def set_month_three
+    end_date = self.batch.end_date
+    return end_date.mon
+  end
+
+  def set_month_two
+    if (self.set_year_three * 12 + self.set_month_three) - (self.set_year_one * 12 + self.set_month_one) > 1
+      if self.set_month_one == 12
+        return 1
+      else
+        return self.set_month_one + 1
+      end
+    end
+  end
+
+  def set_year_one
+    start_date = self.batch.start_date
+    puts start_date.year
+    return start_date.year
+
+  end
+
+  def set_year_two
+    if self.set_month_one == 12
+      puts self.set_year_three
+      return self.set_year_three
+    else
+      puts self.set_year_one
+      return self.set_year_one
+    end
+  end
+
+  def set_year_three
+    end_date = self.batch.end_date
+    puts end_date.year
+    return end_date.year
+  end
+
+
+
+
 
 
 end
