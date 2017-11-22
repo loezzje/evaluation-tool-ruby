7acad57a6450398d0a8e58f6d1dd20031b72206c
loezzje <m.demmendaal@hotmail.com>
2017-11-22 17:26:35 +0100

    add validation evaluation within batch dates

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,18 +2,24 @@ class Evaluation < ApplicationRecord
   belongs_to :student
   validates :date, presence: true
   validate :unique_date, :on => :create
+  validate :date_in_batch, :on => :create,
+    unless: Proc.new { |a| a.date.blank? }
 
 
-def unique_date
-  student = self.student
-  evaluations = student.evaluations
+  def unique_date
+    student = self.student
+    evaluations = student.evaluations
 
-  if evaluations.any? {|e| e.date == self.date }
-    errors.add(:date, "can't be used")
+    if evaluations.any? {|e| e.date == self.date }
+      errors.add(:date, "can't be used")
+    end
   end
-end
 
-
-
+  def date_in_batch
+    batch = self.student.batch
+    if self.date < batch.start_date || self.date > batch.end_date
+      errors.add(:date, "date needs to be within Batch-dates")
+    end
+  end
 
 end
